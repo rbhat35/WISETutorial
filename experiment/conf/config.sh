7c53f3dc0c2ca732729336a9b76425a3f4782acf
Rodrigo A. Lima <ral@gatech.edu>
2020-02-18 13:07:41 -0500

    Set default values.

--- experiment/conf/config.sh
+++ experiment/conf/config.sh
@@ -1,13 +1,14 @@
 # If using bare metal hosts, set with your CloudLab username.
-# If using virtual machines, set with "ubuntu".
+# If using virtual machines (appendix A of the tutorial), set with "ubuntu".
 readonly USERNAME="<FILL IN>"
 
 # If using bare metal hosts, set with "physical".
-# If using virtual machines, set with "vm".
+# If using virtual machines (appendix A of the tutorial), set with "vm".
 readonly HOSTS_TYPE="<FILL IN>"
 
 # Hostnames of each tier.
-readonly CLIENT_HOSTS="<FILL IN>"
+# Example (bare metal host): pc853.emulab.net
+# Example (virtual machine): 10.254.3.128
 readonly WEB_HOSTS="<FILL IN>"
 readonly POSTGRESQL_HOST="<FILL IN>"
 readonly WORKER_HOSTS="<FILL IN>"
@@ -21,23 +22,24 @@ readonly QUEUE_HOSTS="<FILL IN>"
 readonly QUEUE_PORT=9093
 readonly SUB_HOSTS="<FILL IN>"
 readonly SUB_PORT=9094
+readonly CLIENT_HOSTS="<FILL IN>"
 
 # Apache/mod_wsgi configuration.
-readonly APACHE_PROCESSES="<FILL IN>"
-readonly APACHE_THREADSPERPROCESS="<FILL IN>"
+readonly APACHE_PROCESSES=8
+readonly APACHE_THREADSPERPROCESS=4
 
 # Postgres configuration.
-readonly POSTGRES_MAXCONNECTIONS="<FILL IN>"
+readonly POSTGRES_MAXCONNECTIONS=100
 
 # Workers configuration.
-readonly NUM_WORKERS="<FILL IN>"
+readonly NUM_WORKERS=32
 
 # Microservices configuration.
-AUTH_THREADPOOLSIZE="<FILL IN>"
-INBOX_THREADPOOLSIZE="<FILL IN>"
-QUEUE_THREADPOOLSIZE="<FILL IN>"
-SUB_THREADPOOLSIZE="<FILL IN>"
-MICROBLOG_THREADPOOLSIZE="<FILL IN>"
+AUTH_THREADPOOLSIZE=32
+INBOX_THREADPOOLSIZE=32
+QUEUE_THREADPOOLSIZE=32
+SUB_THREADPOOLSIZE=32
+MICROBLOG_THREADPOOLSIZE=32
 
 # Either 0 or 1.
 readonly WISE_DEBUG=0
