686e64521aca4765b2cd177b8e88265870795786
rbhat35 <rbhat35@gatech.edu>
2020-04-07 20:07:49 -0400

    Kill should work now

--- microblog_bench/stress-test/stress_test_1_scripts/start_stress_test.sh
+++ microblog_bench/stress-test/stress_test_1_scripts/start_stress_test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+rm -f pid 
 stress-ng --matrix 0 -t 1h
 
-rm -f pid
+# This saves the PID for use in stop_stress_test
+# Note that, in my testing, this doesn't work, so it can be removed.
 echo "$!" >> pid
--- microblog_bench/stress-test/stress_test_1_scripts/stop_stress_test.sh
+++ microblog_bench/stress-test/stress_test_1_scripts/stop_stress_test.sh
@@ -4,3 +4,6 @@ cd "$(dirname "$(dirname "$(readlink -fm "$0")")")"
 # Stop Stress Test 1.
 xargs kill < pid
 rm pid
+
+# If previous method didn't work, this will definitely kill it
+killall stress-ng
