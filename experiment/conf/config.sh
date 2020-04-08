62f481a28cb0f422edd372fdc1d211adca82ab6a
rbhat35 <rsbhat2@gmail.com>
2020-04-07 20:57:04 -0400

    Adding updated start_stress script

--- experiment/conf/config.sh
+++ experiment/conf/config.sh
@@ -19,23 +19,23 @@ CPUCORES=4
 # Hostnames of each tier.
 # Example (bare metal host): pc853.emulab.net
 # Example (virtual machine): 10.254.3.128
-readonly WEB_HOSTS="10.254.3.107"
-readonly POSTGRESQL_HOST="10.254.1.226"
-readonly WORKER_HOSTS="10.254.2.124"
-readonly MICROBLOG_HOSTS="10.254.0.247"
+readonly WEB_HOSTS="10.254.1.173"
+readonly POSTGRESQL_HOST="10.254.2.94"
+readonly WORKER_HOSTS="10.254.0.83"
+readonly MICROBLOG_HOSTS="10.254.2.27"
 readonly MICROBLOG_PORT=9090
-readonly AUTH_HOSTS="10.254.1.43"
+readonly AUTH_HOSTS="10.254.3.212"
 readonly AUTH_PORT=9091
-readonly INBOX_HOSTS="10.254.0.200"
+readonly INBOX_HOSTS="10.254.0.113"
 readonly INBOX_PORT=9092
-readonly QUEUE_HOSTS="10.254.0.98"
+readonly QUEUE_HOSTS="10.254.1.161"
 readonly QUEUE_PORT=9093
-readonly SUB_HOSTS="10.254.3.231"
+readonly SUB_HOSTS="10.254.1.99"
 readonly SUB_PORT=9094
-readonly CLIENT_HOSTS="10.254.3.206"
+readonly CLIENT_HOSTS="10.254.2.120"
 
 # Hostname of stress-testing nodes
-readonly STRESS_TEST_1="10.254.2.233"
+readonly STRESS_TEST_1="10.254.1.222"
 readonly STRESS_TEST_2="clnode016.clemson.cloudlab.us"
 readonly STRESS_TEST_3="clnode016.clemson.cloudlab.us"
 
